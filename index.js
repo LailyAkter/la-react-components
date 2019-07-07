61ad92cc64884b0899d76c6b064443f007b6ec74
Laily Akter <lailyakter138@gmail.com>
2019-07-07 09:06:09 +0600

    entry point index added

---
+++ index.js
@@ -0,0 +1,12 @@
+import Button from './From/Button';
+import Div from './From/Div';
+import Form from './From/Form';
+import Input from './From/Input';
+import Label from './From/Label';
+import TextArea from './From/TextArea';
+import AlertComp from './AlertComp';
+import ModalComp from './ModalComp';
+import NavComp from './NavComp';
+import ProgresComp from './ProgresComp';
+import SliderComp from './SliderComp';
+module.exports = {Button,Div,Form,Input,Label,TextArea,ModalComp,NavComp,ProgresComp,SliderComp};
